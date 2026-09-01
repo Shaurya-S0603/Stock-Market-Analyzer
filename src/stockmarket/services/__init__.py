45ad4a69134941a742aa3704340abac52f83acdb
Shaurya Singhal <shaurya.s0603@gmail.com>
2026-09-01 12:58:42 +0800

    feat: add portfolio-level purged walk-forward backtesting

--- src/stockmarket/services/__init__.py
+++ src/stockmarket/services/__init__.py
@@ -10,6 +10,7 @@ from .journal import JournalService, TraderCycleSummary
 from .opportunity import OpportunityRanker, RankedOpportunity
 from .paper_strategy import PaperOnlyPortfolioStrategy, PaperStrategyCycleResult
 from .portfolio import PortfolioService, RiskPolicy
+from .portfolio_backtest import PortfolioFoldResult, PortfolioWalkForwardReport, run_portfolio_walk_forward
 from .portfolio_cycle import PortfolioCycleService, PortfolioResearchCycle, PortfolioSignalState
 from .portfolio_optimizer import OptimizedOpportunity, PortfolioOptimizer
 from .rebalancing import PaperRebalancePlan, RebalanceInstruction, build_rebalance_plan
@@ -17,47 +18,14 @@ from .risk import RiskAssessment, RiskEngine, RiskLimits
 from .symbol_stats import SymbolStrategyStats, build_symbol_strategy_stats
 
 __all__ = [
-    "TraderAnalytics",
-    "build_trader_analytics",
-    "cycle_fingerprint",
-    "AITraderConfig",
-    "AITraderService",
-    "TradeDecision",
-    "TraderMode",
-    "AllocationSnapshotRow",
-    "build_allocation_snapshot",
-    "PortfolioAttribution",
-    "SymbolAttribution",
-    "build_portfolio_attribution",
-    "SymbolStrategyStats",
-    "build_symbol_strategy_stats",
-    "PaperRebalancePlan",
-    "RebalanceInstruction",
-    "build_rebalance_plan",
-    "AnalysisRequest",
-    "AnalysisService",
-    "OpportunityRanker",
-    "RankedOpportunity",
-    "OptimizedOpportunity",
-    "PortfolioOptimizer",
-    "build_return_correlation_matrix",
-    "candidate_portfolio_correlation",
-    "ExperimentRecord",
-    "ExperimentRegistry",
-    "DriftReport",
-    "detect_experiment_drift",
-    "PaperOnlyPortfolioStrategy",
-    "PaperStrategyCycleResult",
-    "PortfolioCycleService",
-    "PortfolioResearchCycle",
-    "PortfolioSignalState",
-    "PortfolioService",
-    "RiskPolicy",
-    "RiskAssessment",
-    "RiskEngine",
-    "RiskLimits",
-    "SymbolAnalysis",
-    "WatchlistAnalysis",
-    "JournalService",
-    "TraderCycleSummary",
+    "TraderAnalytics", "build_trader_analytics", "cycle_fingerprint", "AITraderConfig", "AITraderService",
+    "TradeDecision", "TraderMode", "AllocationSnapshotRow", "build_allocation_snapshot", "PortfolioAttribution",
+    "SymbolAttribution", "build_portfolio_attribution", "SymbolStrategyStats", "build_symbol_strategy_stats",
+    "PaperRebalancePlan", "RebalanceInstruction", "build_rebalance_plan", "AnalysisRequest", "AnalysisService",
+    "OpportunityRanker", "RankedOpportunity", "OptimizedOpportunity", "PortfolioOptimizer",
+    "build_return_correlation_matrix", "candidate_portfolio_correlation", "ExperimentRecord", "ExperimentRegistry",
+    "DriftReport", "detect_experiment_drift", "PortfolioFoldResult", "PortfolioWalkForwardReport",
+    "run_portfolio_walk_forward", "PaperOnlyPortfolioStrategy", "PaperStrategyCycleResult", "PortfolioCycleService",
+    "PortfolioResearchCycle", "PortfolioSignalState", "PortfolioService", "RiskPolicy", "RiskAssessment", "RiskEngine",
+    "RiskLimits", "SymbolAnalysis", "WatchlistAnalysis", "JournalService", "TraderCycleSummary",
 ]
